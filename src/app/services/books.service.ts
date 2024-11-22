b43aa79d819299b2ed80145046ffe52206ab6ad3
Alan Rios <alan.rios.hd@gmail.com>
2024-11-22 14:24:16 -0500

    Actualizar la página de inicio y servicios: renombrar a 'Robot APP', agregar tarjeta de contenido y crear servicios para obtener libros y robots.

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BooksService {
-
-  constructor() { }
+export class PokemonService {
+  private apiUrl = 'https://gutendex.com/books/?ids=1,2,3,4,5,6,7,8,9,10';
+  constructor(private http: HttpClient) {}
+  getBooks() {
+    return this.http.get(`${this.apiUrl}`);                              
+  }
 }
--- src/app/services/robots.service.ts
+++ src/app/services/robots.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RobotsService {
-
-  constructor() { }
+export class PokemonService {
+  private apiUrl = 'https://robohash.org/';
+  constructor(private http: HttpClient) {}
+  getRobots(word: string) {
+    word = Math.random().toString(36).substring(2, 12);
+    return this.http.get(`${this.apiUrl}/${word}`);                              
+  }
 }
